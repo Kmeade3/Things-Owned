3c5a58e704516962d6cb49704afdfe57933cdad5
Kmeade3 <kwmeade3@gmail.com>
2020-12-07 14:37:10 -0600

    0

---
+++ Things Owned/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Things Owned
+//
+//  Created by Kenneth W. Meade on 11/10/20.
+//  Copyright © 2020 Kenneth W. Meade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        bruh
+    }
+
+
+}
+
